fc9da26ac5a2d423c1fae20f38bee6c40557fc10
Kacper Gorzelak <kacpergorzelak@outlook.com>
2023-01-24 11:06:10 +0100

    code cleanup

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from wtforms import Form, StringField, PasswordField, DecimalField, IntegerField
 
 
 class RegisterForm(Form):
-    name = StringField('Full Name', [validators.Length(min=1, max=50)])
+    name = StringField('Full Name', [validators.Length(min=2, max=50)])
     email = StringField('Email Address', [validators.Length(min=5, max=50)])
     password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Confirm Password')
